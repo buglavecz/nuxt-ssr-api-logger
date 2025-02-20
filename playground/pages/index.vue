e5de5c9ff7878c85b6eb0b21e991f02a86a1ff56
Peter Buglavecz <buggio.dev@gmail.com>
2025-02-20 23:36:22 +0100

    test: fetch vs useFetch

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -3,7 +3,7 @@ let res = null
 if (import.meta.server) {
   res = await useFetch('https://fake-json-api.mock.beeceptor.com/users')
   // res2 = await fetch('https://fake-json-api.mock.beeceptor.com/users')
-  console.log(res)
+  // console.log(res)
   // console.log(res2)
 }
 </script>
