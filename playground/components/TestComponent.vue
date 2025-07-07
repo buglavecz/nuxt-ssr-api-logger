e83d627cabe6951e5635c83ddbfa75dd1b237ec0
Peter Buglavecz <buggio.dev@gmail.com>
2025-07-07 23:06:38 +0200

    chore: add test component

---
+++ playground/components/TestComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { data } = await useFetch('https://fake-json-api.mock.beeceptor.com/companies/2')
+</script>
+
+<template>
+  <div>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 // const { data } = useAsyncData('users', () => $fetch('https://fake-json-api.mock.beeceptor.com/users'))
+
 const { data: data1 } = await useFetch('https://fake-json-api.mock.beeceptor.com/users')
 const { data: data2 } = await useFetch('https://fake-json-api.mock.beeceptor.com/companies/1')
 </script>
@@ -10,5 +11,8 @@ const { data: data2 } = await useFetch('https://fake-json-api.mock.beeceptor.com
     <pre>{{ data1 }}</pre>
     data 2:
     <pre>{{ data2 }}</pre>
+    <hr>
+    data 3:
+    <TestComponent />
   </div>
 </template>
